da39c722e00cf5f061f8ce4fbdc633f549c02ca5
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-08-12 21:17:05 -0700

    Navigating with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
-    return <div>PageOne</div>;
+    return <div>PageOne
+        <Link to="/pageTwo">Navigate to Page Two</Link>
+    </div>;
 };
 
 const PageTwo = () => {
-    return <div>Page Two <button>Click Me!</button></div>;
+    return <div>Page Two <button>Click Me!</button>
+    <Link to="/">Navigate to Page One</Link>
+    </div>;
 };
 
 const App = () => {
