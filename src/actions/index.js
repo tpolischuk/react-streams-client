b41816a9b8474d7794260a51792b2c3fe67cc83b
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-08-18 20:56:19 -0700

    Creating Streams through Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import streams from '../apis/streams';
 import { SIGN_IN, SIGN_OUT} from './types';
 
 export const signIn = (userId) => {
@@ -11,4 +12,10 @@ export const signOut = () => {
     return {
         type: SIGN_OUT
     };
+};
+
+
+//Crazy syntax, probably gotta learn this
+export const createStream = formValues => async dispatch => {
+    streams.post('streams', formValues);
 };
