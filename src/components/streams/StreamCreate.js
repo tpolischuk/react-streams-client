d14d07027b191e10932ad5be219ce209df0c2c25
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-08-18 21:03:14 -0700

    Creating a stream with REST

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -30,8 +30,8 @@ class StreamCreate extends React.Component {
         );
     }
 
-    onSubmit(formValues) {
-        console.log(formValues);
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
     }
 
     render() {
@@ -62,7 +62,9 @@ const validate = (formValues) => {
 
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
