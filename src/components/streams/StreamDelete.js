c0a06457f4edb4e11704c3b529de9d51fee6f130
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-08-31 20:45:07 -0700

    Fetching the deletion stream

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,31 +1,43 @@
 import Modal from '../Modal';
+import { connect } from 'react-redux';
 import React from 'react';
 import history from '../../history';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
 
-    const actions = (
+    //components should be in charge of retrieving their own data
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
 
-        // You can also use <></>
-        <React.Fragment>
-            <button className="ui button negative">
-                Delete
-            </button>
-            <button className="ui button">Cancel</button>
-        </React.Fragment >
-    );
+    renderActions() {
+        return (
+            // You can also use <></>
+            <React.Fragment>
+                <button className="ui button negative">
+                    Delete
+                </button>
+                <button className="ui button">Cancel</button>
+            </React.Fragment >
+        );
+    };
 
-    return (
-        <div>
-            StreamDelete
-            <Modal
-                title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
-                actions={actions}
-                onDismiss={() => history.push('/')}
-            />
-        </div>
-    );
-};
 
-export default StreamDelete;
+    render() {
+        return (
+            <div>
+                StreamDelete
+                < Modal
+                    title="Delete Stream"
+                    content="Are you sure you want to delete this stream?"
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push('/')}
+                />
+            </div >
+        );
+    }
+
+}
+
+export default connect(null, { fetchStream })(StreamDelete);
